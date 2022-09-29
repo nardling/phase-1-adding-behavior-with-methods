c49f4b710e109e02452113d835cc11222e1710aa
David <david.v.bernardo@gmail.com>
2022-09-29 10:17:55 -0500

    commit tree e7713ab4d074a01c93a9cd8b4b44df6e617fdeff

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+class Speaker {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    speak() {
+        return `${this.name} says `
+    }
+}
+
+class Cat extends Speaker {
+    speak() {
+        return super.speak() + "meow!"
+    }
+}
+
+class Dog extends Speaker {
+    speak() {
+        return super.speak() + "woof!"
+    }
+}
+
+class Bird extends Speaker {
+    speak() {
+        if (this.sex === 'male')
+            return `It's me! ${this.name}, the parrot!`
+        else
+            return super.speak() + "squawk!"
+    }
+}
